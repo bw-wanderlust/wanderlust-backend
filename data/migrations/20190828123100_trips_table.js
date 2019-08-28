0c2a293ab2c797c1595bec2a3e9a416b2081de33
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-08-28 13:22:04 -0500

    trips table build out and participants tablefile created

---
+++ data/migrations/20190828123100_trips_table.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("trips", table => {
+    table.increments();
+    table.string("title").notNullable();
+    table.string("description");
+    table
+      .integer("organizerId")
+      .unsigned()
+      .references("id")
+      .inTable("users");
+    table.string("location").notNullable();
+    table.string("cost");
+    table.boolean("isFree").defaultTo(false);
+    table.date("startDate").notNullable();
+    table.date("endDate").notNollable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("trips");
+};
