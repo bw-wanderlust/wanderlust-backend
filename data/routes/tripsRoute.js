8141c4135ea91b335c3beaf19f4dfcfe3bd7408b
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-08-28 14:30:04 -0500

    fixed user route

---
+++ data/routes/tripsRoute.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const router = express.Router();
+
+const helper = require("../helpers/userDB");
+const bcrypt = require("bcrypt");
+
+const { authenticate, generateToken } = require("../middleware/authMiddleware");
+
+const register = (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
+  if (creds.username && creds.password && creds.department) {
+    helper
+      .addUser(creds)
+      .then(ids => {
+        const id = ids[0];
+        userDb
+          .getUser(id)
+          .then(user => {
+            const token = generateToken(user);
+            res.status(201).json({ token });
+          })
+          .catch(err => {
+            res.status(500).json("Failed to authenticate user");
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Failed to resister user" });
+      });
+  } else {
+    res.status(404).json({ message: "Missing username/password/department" });
+  }
+};
+
+const login = (req, res) => {
+  // implement user login
+  const creds = req.body;
+  if (creds.username && creds.password) {
+    helper
+      .login(creds.username)
+      .then(user => {
+        if (
+          user.password &&
+          bcrypt.compareSync(creds.password, user.password)
+        ) {
+          const token = generateToken(user);
+          res.status(201).json({ token });
+        } else {
+          res.status(500).json({ message: "Failed to authenticate" });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ message: "Failed to login" });
+      });
+  } else {
+    res.status(404).json({ message: "username/password Missing" });
+  }
+};
+
+const allUsers = (req, res) => {
+  helper
+    .getUsers()
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: err.message
+      });
+    });
+};
+
+const userById = (req, res) => {
+  const { id } = req.params;
+  helper
+    .getUsers(id)
+    .then(row => {
+      console.log("user by id endpoint", row);
+      !row[0]
+        ? res.json(row)
+        : res.status(404).json({
+            error: "User with that ID not found"
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "getting user by id failed"
+      });
+    });
+};
+
+const editUser = (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  let password = user.password;
+  console.log("no hash :", user);
+  if (password) {
+    user.password = bcrypt.hashSync(password, 12);
+    console.log("w/hash :", user);
+  }
+
+  helper
+    .updateUser(id, user)
+    .then(number => {
+      res.json(number);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Internal router issue with the edit of the user."
+      });
+    });
+};
+
+const removeUser = (req, res) => {
+  const { id } = req.params;
+  helper
+    .deleteUser(id)
+    .then(number => {
+      !number
+        ? res.status(404).json({
+            message: "user Not Found"
+          })
+        : res.json({
+            message: "Its gone!"
+          });
+    })
+    .catch(err => {
+      res.status(500).send({
+        error: err
+      });
+    });
+};
+
+router.get("/trips", allUsers);
+router.get("/trips/:id", userById);
+router.put("/trips/:id", editUser);
+router.delete("/trips/:id", editUser);
+
+module.exports = router;
--- data/routes/usersRoute.js
+++ data/routes/usersRoute.js
@@ -1,84 +1,143 @@
 const express = require("express");
 const router = express.Router();
 
-const helper = require("../helpers/tripsDB");
+const helper = require("../helpers/userDB");
+const bcrypt = require("bcrypt");
 
-const allTrips = (req, res) => {
+const { authenticate, generateToken } = require("../middleware/authMiddleware");
+
+const register = (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
+  if (creds.username && creds.password && creds.department) {
+    helper
+      .addUser(creds)
+      .then(ids => {
+        const id = ids[0];
+        userDb
+          .getUser(id)
+          .then(user => {
+            const token = generateToken(user);
+            res.status(201).json({ token });
+          })
+          .catch(err => {
+            res.status(500).json("Failed to authenticate user");
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Failed to resister user" });
+      });
+  } else {
+    res.status(404).json({ message: "Missing username/password/department" });
+  }
+};
+
+const login = (req, res) => {
+  // implement user login
+  const creds = req.body;
+  if (creds.username && creds.password) {
+    helper
+      .login(creds.username)
+      .then(user => {
+        if (
+          user.password &&
+          bcrypt.compareSync(creds.password, user.password)
+        ) {
+          const token = generateToken(user);
+          res.status(201).json({ token });
+        } else {
+          res.status(500).json({ message: "Failed to authenticate" });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ message: "Failed to login" });
+      });
+  } else {
+    res.status(404).json({ message: "username/password Missing" });
+  }
+};
+
+const allUsers = (req, res) => {
   helper
-    .getTrips()
+    .getUsers()
     .then(users => {
       res.status(201).json(users);
     })
     .catch(err => {
       res.status(500).send({
-        error: "getting trips failed"
+        error: err.message
       });
     });
 };
 
-const tripById = (req, res) => {
+const userById = (req, res) => {
   const { id } = req.params;
   helper
-    .getTrips(id)
+    .getUsers(id)
     .then(row => {
+      console.log("user by id endpoint", row);
       !row[0]
         ? res.json(row)
         : res.status(404).json({
-            error: "trip with that ID not found"
+            error: "User with that ID not found"
           });
     })
     .catch(err => {
       res.status(500).json({
-        error: "getting trip by id failed"
+        error: "getting user by id failed"
       });
     });
 };
 
-const editTrip = (req, res) => {
+const editUser = (req, res) => {
   const { id } = req.params;
   const user = req.body;
   let password = user.password;
-  // console.log("no hash :", user);
+  console.log("no hash :", user);
   if (password) {
     user.password = bcrypt.hashSync(password, 12);
-    // console.log("w/hash :", user);
+    console.log("w/hash :", user);
   }
 
   helper
-    .updateTrip(id, user)
+    .updateUser(id, user)
     .then(number => {
       res.json(number);
     })
     .catch(err => {
       res.status(500).json({
-        error: "Internal router issue with the edit of the trip."
+        error: "Internal router issue with the edit of the user."
       });
     });
 };
 
-const removeTrip = (req, res) => {
+const removeUser = (req, res) => {
   const { id } = req.params;
   helper
-    .deleteTrip(id)
+    .deleteUser(id)
     .then(number => {
       !number
         ? res.status(404).json({
-            message: "trip Not Found"
+            message: "user Not Found"
           })
         : res.json({
             message: "Its gone!"
           });
     })
     .catch(err => {
-      res.status(500).json({
-        error: "Internal router issue with the deletion of the trip."
+      res.status(500).send({
+        error: err
       });
     });
 };
 
-router.get("/trips", allTrips);
-router.get("/trips/:id", tripById);
-router.put("/trips/:id", editTrip);
-router.delete("/trips/:id", removeTrip);
+router.post("/users/register", register);
+router.post("/users/login", login);
+router.get("/users", allUsers);
+router.get("/users/:id", userById);
+router.put("/users/:id", editUser);
+router.delete("/users/:id", removeUser);
 
 module.exports = router;
