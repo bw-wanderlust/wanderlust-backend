4d515f6aff630491b7327bcd99f69dfb548b4748
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-08-28 14:57:22 -0500

    created seeds for trips

---
+++ data/seeds/01_users.js
@@ -0,0 +1,23 @@
+const faker = require("faker");
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("trips")
+    .del()
+    .then(function() {
+      let trips = [];
+      for (let i = 0; i < 10; i++) {
+        trips.push({
+          title: faker.lorem.sentence(),
+          description: faker.lorem.paragraph(),
+          organizerId: 1,
+          location: faker.address.streetAddress(),
+          cost: faker.finance.amount().toString(),
+          isFree: false,
+          startDate: faker.date.recent(),
+          endDate: faker.date.future()
+        });
+      }
+      return knex("table_name").insert(trips);
+    });
+};
